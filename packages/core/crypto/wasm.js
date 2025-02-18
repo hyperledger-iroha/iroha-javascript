95d5210a1e7bff49e1ce34e1a3ace541b937c981
0x009922 <43530070+0x009922@users.noreply.github.com>
2025-02-18 16:01:02 +0900

    docs(core): extend .wasm import error message
    Signed-off-by: 0x009922 <43530070+0x009922@users.noreply.github.com>

--- packages/core/crypto/wasm.js
+++ packages/core/crypto/wasm.js
@@ -12,12 +12,12 @@ try {
   if (!(typeof wasm === 'object' && typeof wasm.__wbindgen_start === 'function')) {
     throw 'not module'
   }
-} catch {
+} catch (errImport) {
   try {
     const response = fetch(new URL('./wasm/iroha_crypto_wasm.wasm', import.meta.url))
     const { instance } = await WebAssembly.instantiateStreaming(response, imports)
     wasm = instance.exports
-  } catch {
+  } catch (errFetch) {
     try {
       const fs = await import(
         // turn off Vite warning https://vite.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility
@@ -28,12 +28,19 @@ try {
       const wasmModule = new WebAssembly.Module(bytes)
       const wasmInstance = new WebAssembly.Instance(wasmModule, imports)
       wasm = wasmInstance.exports
-    } catch {
+    } catch (errNodeFs) {
       console.error(
         'Unable to import/load `iroha_crypto_wasm.wasm`. There are a few ways to fix it:\n' +
           ` - If you are using a bundler such as Vite, consider using "vite-plugin-wasm" to enable direct imports of \`.wasm\` modules. ` +
           `This documentation could help: https://vite.dev/guide/features#webassembly\n` +
-          ` - If you are using importing directly in Browser, consider using https://github.com/guybedford/es-module-shims`,
+          ` - If you are using importing directly in Browser, consider using https://github.com/guybedford/es-module-shims\n` +
+          `Below are collected errors:\n`,
+        ` - when tried import() with .wasm:`,
+        errImport,
+        `\n - when tried fetch():`,
+        errFetch,
+        `\n - when tried import('node:fs') + readFileSync:`,
+        errNodeFs,
       )
       throw new TypeError('Could not import `iroha_crypto_wasm.wasm`')
     }
